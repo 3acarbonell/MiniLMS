0c4d3831a9dc658b380bf22a187bee41235afbcc
Allan Carbonell <allan.carbonell@virginiogomez.cl>
2026-05-17 22:50:34 -0400

    fix static files dir

--- MiniLMS/settings.py
+++ MiniLMS/settings.py
@@ -127,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'MiniLMS/LandingPage/static/'),
+]
 
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
