73ebe3f338a50afae23f8886989c21ef2f5673d8
Allan Carbonell <allan.carbonell@virginiogomez.cl>
2026-05-18 01:49:42 -0400

    Fix routes

--- MiniLMS/settings.py
+++ MiniLMS/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.StaticFilesStorage'
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
