559e859803d8a9067bc1fd7144a707eaa160040e
Allan Carbonell <allan.carbonell@virginiogomez.cl>
2026-05-17 23:35:30 -0400

    fix migrations

--- MiniLMS/settings.py
+++ MiniLMS/settings.py
@@ -127,15 +127,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'LandingPage' / 'static',
-]
+STATIC_URL = 'static/'
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ python manage.py collectstatic --no-input
 
 # Apply any outstanding database migrations
 python manage.py makemigrations
-python manage.py migrate
+python manage.py migrate --noinput
+
+# Population
 python manage.py populate_users
 python manage.py populate_courses
