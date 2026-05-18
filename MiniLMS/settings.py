a483155c0b859ba692ace62fee4d163a0c933717
Allan Carbonell <allan.carbonell@virginiogomez.cl>
2026-05-17 23:01:47 -0400

    change static files dir

--- MiniLMS/settings.py
+++ MiniLMS/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -130,7 +130,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'MiniLMS/LandingPage/static/'),
+    BASE_DIR / 'LandingPage' / 'static',
 ]
 
 if not DEBUG:
