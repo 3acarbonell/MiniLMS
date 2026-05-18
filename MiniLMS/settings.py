818519d728a89c2d37aeb701f32ec69850e71669
Allan Carbonell <allan.carbonell@virginiogomez.cl>
2026-05-17 23:54:47 -0400

    revert changes in static files

--- MiniLMS/settings.py
+++ MiniLMS/settings.py
@@ -127,9 +127,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'LandingPage' / 'static',
+]
 
 if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
     SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
