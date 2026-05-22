d8bf3887c42b4dd7b50634551013b7fd3f9d25ca
Allan Carbonell <allan.carbonell@virginiogomez.cl>
2026-05-21 22:31:14 -0400

    add domain to allowed hosts

--- MiniLMS/settings.py
+++ MiniLMS/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'minilms-hz2z.onrender.com']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
